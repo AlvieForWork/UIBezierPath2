329016b6f7415380b266fc4c8c2e71e0318ecba8
Alvie <alvieforwork@gmail.com>
2021-08-20 21:47:45 +0800

    Initial Commit

---
+++ UIBezierPath2/UIBezierPath2App.swift
@@ -0,0 +1,17 @@
+//
+//  UIBezierPath2App.swift
+//  UIBezierPath2
+//
+//  Created by worker on 2021/8/20.
+//
+
+import SwiftUI
+
+@main
+struct UIBezierPath2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
